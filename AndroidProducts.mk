731a695fe4215bab781fe998498036771ebb5d70
RyuujiX <saputradenny712@gmail.com>
2022-06-28 22:08:18 +0700

    X01BD: Bringup Nusantara 11
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/nad_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    nad_X01BD-user \
+    nad_X01BD-userdebug \
+    nad_X01BD-eng
